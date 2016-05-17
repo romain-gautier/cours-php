0fba2aec0c2805e10194dee17abad71fe710f588
Romain Gautier <romain.photog@gmail.com>
2016-05-17 18:12:24 +0200

    mise a jours méthodes Article

--- blog_poo/app/Article.php
+++ blog_poo/app/Article.php
@@ -82,16 +82,25 @@ class Article
         $reponse = $req->query('SELECT id, titre_article, contenu_article, auteur_article, DATE_FORMAT(date_ajout, \'[%d/%m/%Y %H:%i:%s] \') AS date_ajout_fr FROM articles ORDER BY ID DESC');
         ?>
         <table class="table table-hover">
-        <?php while ($donnees = $reponse->fetch()) { ?>
-        <tr>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
-            <td class="text-left"> <?php echo "<a href='modif.php?id=" . htmlspecialchars($donnees["id"]) . "'> Modifier </a>"; ?> </td>
-            <td class="text-left"> <?php echo "<a href='supprimer.php?id=" . htmlspecialchars($donnees["id"]) . "'> Supprimer </a>"; ?> </td>
-            <td class="text-left"> <?php echo "<a href='post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
-        </tr>
+            <tr>
+                <th>Date</th>
+                <th>Titre</th>
+                <th>Contenu</th>
+                <th>Auteur</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php while ($donnees = $reponse->fetch()) { ?>
+            <tr>
+                <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
+                <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
+                <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
+                <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
+                <td class="text-left"> <?php echo "<a href='post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
+                <td class="text-left"> <?php echo "<a href='modif.php?id=" . htmlspecialchars($donnees["id"]) . "'> Modifier </a>"; ?> </td>
+                <td class="text-left"> <?php echo "<a href='supprimer.php?id=" . htmlspecialchars($donnees["id"]) . "'> Supprimer </a>"; ?> </td>
+            </tr>
     <?php  }
         $reponse->closeCursor();
         ?>
