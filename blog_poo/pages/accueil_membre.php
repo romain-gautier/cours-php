f48a20483bb34edd498acaf2652330d68e5e221e
Romain Gautier <romain.photog@gmail.com>
2016-05-25 11:53:30 +0200

    mise à jour de la zone d'accueil en fonction de l'utilisateur connecté

---
+++ blog_poo/pages/accueil_membre.php
@@ -0,0 +1,23 @@
+<table class="table table-hover">
+    <tr>
+        <th>Date</th>
+        <th>Titre</th>
+        <th>Contenu</th>
+        <th>Auteur</th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <?php while ($donnees = $reponse->fetch()) { ?>
+        <tr>
+            <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
+            <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
+            <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
+            <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
+            <td class="text-left"> <?php echo "<a href='post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
+            <td class="text-left"> <?php echo "<a href='modif.php?id=" . htmlspecialchars($donnees["id"]) . "'> Modifier </a>"; ?> </td>
+        </tr>
+    <?php }
+    $reponse->closeCursor();
+    ?>
+</table>
--- blog_poo/pages/public/accueil.php
+++ blog_poo/pages/public/accueil.php
@@ -24,89 +24,46 @@ $req = $connect->connection();
 $reponse = $req->query('SELECT id, titre_article, contenu_article, auteur_article, DATE_FORMAT(date_ajout, \'[%d/%m/%Y %H:%i:%s] \') AS date_ajout_fr FROM articles ORDER BY ID DESC');
 ?>
 
-<?php if($_SESSION['loginMembre'] === 'Romain') {
-    ?>
-<table class="table table-hover">
-    <tr>
-        <th>Date</th>
-        <th>Titre</th>
-        <th>Contenu</th>
-        <th>Auteur</th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <?php while ($donnees = $reponse->fetch()) { ?>
-        <tr>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
-            <td class="text-left"> <?php echo "<a href='post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
-            <td class="text-left"> <?php echo "<a href='modif.php?id=" . htmlspecialchars($donnees["id"]) . "'> Modifier </a>"; ?> </td>
-            <td class="text-left"> <?php echo "<a href='../supprimer.php?id=" . htmlspecialchars($donnees["id"]) . "'> Supprimer </a>"; ?> </td>
-        </tr>
-    <?php  }
-    $reponse->closeCursor();
-    ?>
-</table>
-
-<?php } elseif ($_SESSION['loginMembre'] === 'Karim') {
-    ?>
-<table class="table table-hover">
-    <tr>
-        <th>Date</th>
-        <th>Titre</th>
-        <th>Contenu</th>
-        <th>Auteur</th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <?php while ($donnees = $reponse->fetch()) { ?>
-        <tr>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
-            <td class="text-left"> <?php echo "<a href='pages/public/post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
-            <td class="text-left"> <?php echo "<a href='pages/public/modif.php?id=" . htmlspecialchars($donnees["id"]) . "'> Modifier </a>"; ?> </td>
-        </tr>
-    <?php  }
-    $reponse->closeCursor();
-    ?>
-</table>
-<?php } else {
-    ?>
-<table class="table table-hover">
-    <tr>
-        <th>Date</th>
-        <th>Titre</th>
-        <th>Contenu</th>
-        <th>Auteur</th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <?php while ($donnees = $reponse->fetch()) { ?>
-        <tr>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
-            <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
-            <td class="text-left"> <?php echo "<a href='pages/public/post.php?id=" . htmlspecialchars($donnees["id"]) . "'> Afficher </a>"; ?> </td>
-        </tr>
-    <?php  }
-    $reponse->closeCursor();
-    ?>
-</table>
 <?php
 
-} ?>
+    if(isset($_SESSION['loginMembre'])) {
+
+        if ($_SESSION['loginMembre'] === 'Romain') {
+            include '../accueil_admin.php';
+         } elseif ($_SESSION['loginMembre'] === 'Karim') {
+            include '../accueil_membre.php';
+         } elseif ($_SESSION['loginMembre'] === 'Guest') {
+            include '../accueil_guest.php';
+        }
+    } else {
+        ?>
+            <p>Bonjour cher visiteur, pour accéder aux fonctions du blog vous devez <a href="../../index.php">vous connecter.</a></p>
+            <table class="table table-hover">
+                <tr>
+                    <th>Date</th>
+                    <th>Titre</th>
+                    <th>Contenu</th>
+                    <th>Auteur</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <?php while ($donnees = $reponse->fetch()) { ?>
+                    <tr>
+                        <td class="text-left"> <?php echo htmlspecialchars($donnees['date_ajout_fr']); ?> </td>
+                        <td class="text-left"> <?php echo htmlspecialchars($donnees['titre_article']); ?> </td>
+                        <td class="text-left"> <?php echo htmlspecialchars($donnees['contenu_article']); ?> </td>
+                        <td class="text-left"> <?php echo htmlspecialchars($donnees['auteur_article']); ?> </td>
+                    </tr>
+                <?php }
+                $reponse->closeCursor();
+                ?>
+            </table>
+<?php
+    }   ?>
+
 
 <a href="../deco.php">Déconnection</a>
 
 </body>
 </html>
-
-
