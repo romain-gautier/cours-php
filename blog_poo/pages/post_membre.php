8bd98126ba724e9a3af44fc39ecd1466be718910
Romain Gautier <romain.photog@gmail.com>
2016-05-25 18:44:24 +0200

    finalisation des autorisations

---
+++ blog_poo/pages/post_membre.php
@@ -0,0 +1,11 @@
+<?php
+$recup_com = $bdd->prepare('SELECT id, auteur_com, contenu_com, id_article, DATE_FORMAT(date_com, \'[%d/%m/%Y %H:%i:%s] \') AS date_com_fr FROM commentaires WHERE id_article = :id ORDER BY date_com_fr DESC' );
+$recup_com->execute(array("id" => $_GET["id"]));
+while ($donnees_com = $recup_com->fetch()) { ?>
+    De : <?php echo $donnees_com['auteur_com']; ?> <br>
+    Le : <?php echo $donnees_com['date_com_fr']; ?> <br>
+    Commentaire : <?php echo $donnees_com['contenu_com']; ?> <br>
+    <a href="modif_com.php?id=<?php echo $donnees_com['id']; ?>">Modifier le commentaire</a><br>
+<?php }
+$recup_com->closeCursor();
+?>
--- blog_poo/pages/public/post.php
+++ blog_poo/pages/public/post.php
@@ -59,17 +59,26 @@ session_start();
     <hr>
 
     <h4>Commentaires :</h4>
-
     <?php
-    $recup_com = $bdd->prepare('SELECT id, auteur_com, contenu_com, id_article, DATE_FORMAT(date_com, \'[%d/%m/%Y %H:%i:%s] \') AS date_com_fr FROM commentaires WHERE id_article = :id ORDER BY date_com_fr DESC' );
-    $recup_com->execute(array("id" => $_GET["id"]));
-    while ($donnees_com = $recup_com->fetch()) { ?>
-        De : <?php echo $donnees_com['auteur_com']; ?> <br>
-        Le : <?php echo $donnees_com['date_com_fr']; ?> <br>
-        Commentaire : <?php echo $donnees_com['contenu_com']; ?> <br>
-        <a href="modif_com.php?id=<?php echo $donnees_com['id']; ?>">Modifier le commentaire</a> <a href="supr_com.php?id=<?php echo $donnees_com['id']; ?>">Supprimer le commentaire</a> <br>
-    <?php }
-    $recup_com->closeCursor();
+    if(isset($_SESSION['loginMembre'])) {
+
+        if ($_SESSION['loginMembre'] === 'Romain') {
+            include '../post_admin.php';
+        } elseif ($_SESSION['loginMembre'] === 'Karim') {
+            include '../post_membre.php';
+        } elseif ($_SESSION['loginMembre'] === 'Guest') {
+            include '../post_guest.php';
+        }
+    } else {
+$recup_com = $bdd->prepare('SELECT id, auteur_com, contenu_com, id_article, DATE_FORMAT(date_com, \'[%d/%m/%Y %H:%i:%s] \') AS date_com_fr FROM commentaires WHERE id_article = :id ORDER BY date_com_fr DESC' );
+$recup_com->execute(array("id" => $_GET["id"]));
+while ($donnees_com = $recup_com->fetch()) { ?>
+    De : <?php echo $donnees_com['auteur_com']; ?> <br>
+    Le : <?php echo $donnees_com['date_com_fr']; ?> <br>
+    Commentaire : <?php echo $donnees_com['contenu_com']; ?> <br>
+<?php }
+$recup_com->closeCursor();
+ }
     ?>
     <br>
 
